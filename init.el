28a856abb5a69cb558bebe509ba6c7f49e5ec78c
guo liboa <guolibao1982@gmail.com>
2020-04-10 18:33:46 +0800

    add the package needed

--- init.el
+++ init.el
@@ -19,7 +19,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (auctex use-package))))
+ '(package-selected-packages
+   (quote
+    (smex htmlize plantuml-mode highlight-symbol auctex use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
